b94b8712b1ea1bcb1a185b0405535a672aa6f373
ChandupaJay1 <chandupajayalath33@gmail.com>
2024-09-02 01:34:07 +0530

    bugs fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,78 +6,70 @@ const User = require('../models/User')
 
 
 // @desc Get all users
-// @desc GET /users
+// @route GET /users
 // @access Private
-
-const getAllUser = asyncHandler(async (req, res) => {
-
+const getAllUsers = asyncHandler(async (req, res) => {
+    // Get all users from MongoDB
     const users = await User.find().select('-password').lean()
-        if(!user) {
-            return res.status(400).json({ message: 'No users found'})
-        }
 
-        res.json(users)
+    // If no users 
+    if (!users?.length) {
+        return res.status(400).json({ message: 'No users found' })
+    }
 
+    res.json(users)
 })
 
-// @desc Create new users
-// @desc POST /users
+// @desc Create new user
+// @route POST /users
 // @access Private
-
 const createNewUser = asyncHandler(async (req, res) => {
+    const { username, password, roles } = req.body
 
-    const {username, paassword, roles} = req.body
-
-    //Confirm data
-    if (!username || !password || !Array.isArray(roles) || !roles.length ) {
-        return res.status(400).json({ message: 'All fields are required'})
-    }
-    
-
-    // Check for duplicate
-    const duplicate = await User.findOne({ username }).lean().exec()
-    
-    if (duplicate) {
-        return res.status(409).json({ message: 'All fields are required'})
-    }
-
-
-    // Hash password
-    const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
-
-    const userObject = { username, "password":hashedPwd, roles }
-
-
-    // Create and store new user
-    const user = await User.create(userObject)
-
-    if (user) { //created
-        res.status(201).json({ message: 'New user ${username} created' })
-    }else{
-        res.status(400).json({ message: 'Invalid user data received' })
-    }
-
-})
-
-
-// @desc Update a users
-// @desc PATCH /users
-// @access Private
-
-const updateUser = asyncHandler(async (req, res) => {
-    const {od, username, roles, active, password } = req.body
-
-    //confirm data
-    if (!id || !username || !Array.isArray(roles) || !eolwa.length || typeof active !== 'boolean') {
+    // Confirm data
+    if (!username || !password || !Array.isArray(roles) || !roles.length) {
         return res.status(400).json({ message: 'All fields are required' })
     }
 
+    // Check for duplicate username
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    // Hash password 
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, "password": hashedPwd, roles }
+
+    // Create and store new user 
+    const user = await User.create(userObject)
+
+    if (user) { //created 
+        res.status(201).json({ message: `New user ${username} created` })
+    } else {
+        res.status(400).json({ message: 'Invalid user data received' })
+    }
+})
+
+// @desc Update a user
+// @route PATCH /users
+// @access Private
+const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, roles, active, password } = req.body
+
+    // Confirm data 
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({ message: 'All fields except password are required' })
+    }
+
+    // Does the user exist to update?
     const user = await User.findById(id).exec()
 
     if (!user) {
-        return res.status(400).json({ message:'User not found' })
+        return res.status(400).json({ message: 'User not found' })
     }
-
 
     // Check for duplicate 
     const duplicate = await User.findOne({ username }).lean().exec()
@@ -91,53 +83,50 @@ const updateUser = asyncHandler(async (req, res) => {
     user.roles = roles
     user.active = active
 
-
     if (password) {
-        // hash password
-        user.password = await bcrypt.hash(password, 10) // salt rounds
+        // Hash password 
+        user.password = await bcrypt.hash(password, 10) // salt rounds 
     }
 
     const updatedUser = await user.save()
 
-    res.json({ message: '${updatedUser.username} updated' })
-
+    res.json({ message: `${updatedUser.username} updated` })
 })
 
-
-
-// @desc Delete a users
-// @desc DELETE /users
+// @desc Delete a user
+// @route DELETE /users
 // @access Private
-
 const deleteUser = asyncHandler(async (req, res) => {
     const { id } = req.body
 
-    if(!id) {
+    // Confirm data
+    if (!id) {
         return res.status(400).json({ message: 'User ID Required' })
     }
 
-    const notes = await Note.findOne({ user: id }).lean().exec()
-    if (notes?.length) {
+    // Does the user still have assigned notes?
+    const note = await Note.findOne({ user: id }).lean().exec()
+    if (note) {
         return res.status(400).json({ message: 'User has assigned notes' })
     }
 
+    // Does the user exist to delete?
     const user = await User.findById(id).exec()
 
-        if (!user) {
-            return res.status(400).json({ message: 'User not found' })
-        }
+    if (!user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
 
-        const result = await user.deleteOne()
+    const result = await user.deleteOne()
 
-        const reply = `Username ${result.username} with ID ${result._id} deleted`
-    
-        res.json(reply)
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
 
+    res.json(reply)
 })
 
 module.exports = {
-    getAllUser,
+    getAllUsers,
     createNewUser,
     updateUser,
-    deleteUser,
+    deleteUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const usersController = require('../controllers/userController')
+const userController = require('../controllers/userController'); // Add this line
 
 router.route('/')
     .get(userController.getAllUsers)
