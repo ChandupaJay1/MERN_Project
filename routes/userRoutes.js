262f3f2edb33cf89f4d832fc29a3f203d39aea50
ChandupaJay1 <chandupajayalath33@gmail.com>
2024-09-02 22:29:42 +0530

    some update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/userController'); // Add this line
+const userController = require('../controllers/userController'); 
 
 router.route('/')
     .get(userController.getAllUsers)
